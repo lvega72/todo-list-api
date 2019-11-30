b366257e9b35637c82aeeb4dff3549eb39586540
lvega <lvega@lvega.com>
2019-11-29 22:17:14 -0600

    solutions

--- server.js
+++ server.js
@@ -1,27 +1,76 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var app = express();
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var todoList = [
-    {
-        id: 1,
-        todo: "Implement a REST API"
-    }
+let counter = 1;
+let todoList = [
+  {
+    id: 1,
+    todo: "Implement a REST API"
+  }
 ];
 
 // GET /api/todos
+app.get("/api/todos", function(request, response, next) {
+  response.json(todoList);
+});
 
 // GET /api/todos/:id
+app.get("/api/todos/:id", function(request, response, next) {
+  let todoItem = todoList.find(function(element) {
+    return element.id.toString() === request.params.id;
+  });
+
+  response.json(todoItem);
+});
 
 // POST /api/todos
+app.post("/api/todos", function(request, response, next) {
+  counter++;
+
+  let todoItem = request.body;
+  todoItem = { id: counter, ...todoItem }; // note we are using the spread operator (...)
+
+  todoList.push(todoItem);
+
+  response.json(todoItem);
+});
 
 // PUT /api/todos/:id
+app.put("/api/todos/:id", function(request, response, next) {
+  let todoItem = todoList.find(function(element) {
+    return element.id.toString() === request.params.id;
+  });
+
+  //   let newPropertyEntries = Object.entries(request.body)[0]; // just the first key/value pair
+  //   todoItem[newPropertyEntries[0]] = newPropertyEntries[1];
+
+  var newTodoData = request.body;
+  var newTodoKeys = Object.keys(newTodoData);
+
+  newTodoKeys.forEach(function(key) {
+    todoItem[key] = newTodoData[key];
+  });
+
+  response.json(todoItem);
+});
 
 // DELETE /api/todos/:id
+app.delete("/api/todos/:id", function(request, response, next) {
+  let indexToRemove = todoList.findIndex(function(todoItem) {
+    return todoItem.id.toString() === request.params.id;
+  });
 
-app.listen(3000, function(){
-    console.log('Todo List API is now listening on port 3000...');
-})
+  if (indexToRemove >= 0) {
+    todoList.splice(indexToRemove, 1);
+  }
+
+  response.sendStatus(200);
+});
+
+app.listen(3000, function() {
+  console.log("Todo List API is now listening on port 3000...");
+});
